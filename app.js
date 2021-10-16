eca247d4cd05d29ac8a7ad80b2fd8d3df31ceaee
Santiago L Martinez <slm17slm17@gmail.com>
2021-10-15 23:37:31 -0300

    ahora si metimos el css
    commit 3

--- app.js
+++ app.js
@@ -2,14 +2,35 @@
 
 //Funciones
 
-function limpiar(){
+function limpiar() {
     document.getElementById('FormCalculadora').reset();
 
 }
 
-function sumar(){
-var num1 = parseInt(document.getElementById('num1').value);
-var num2 = parseInt(document.getElementById('num2').value);
+function sumar() {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
 
-document.getElementById('Resultado').innerHTML = ('El resultado es: ') + num1+num2 ;
+    document.getElementById('Resultado').innerHTML =  num1 + num2;
+}
+
+function resta() {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+
+    document.getElementById('Resultado').innerHTML =  num1 - num2;
+}
+
+function division() {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+
+    document.getElementById('Resultado').innerHTML =   num1 / num2;
+}
+
+function multiplicacion() {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+
+    document.getElementById('Resultado').innerHTML = num1 * num2;
 }
